71a1acf6d97fcc79b0c7d7d722c0c4975c84367c
Tommy Chu <chutommy@fit.cvut.cz>
2023-05-13 23:06:10 +0200

    Main separation

--- src/main.cpp
+++ src/main.cpp
@@ -3,44 +3,53 @@
  * @date 3.5.23
  */
 
-
-#include "file/File.h"
-#include "file/TXTFile.h"
-#include "file/CSVFile.h"
-#include "file/CPPFile.h"
-#include "query/Query.h"
-#include "query/ANDQuery.h"
-#include "query/ORQuery.h"
+#include "File.h"
+#include "TXTFile.h"
+#include "CSVFile.h"
+#include "CPPFile.h"
+#include "Query.h"
+#include "ANDQuery.h"
+#include "ORQuery.h"
 
 #include <filesystem>
 #include <iostream>
 #include <fstream>
 #include <cstring>
 
-int main(int argc, char** argv)
+void load_and_print(std::shared_ptr<MainQuery>& query)
 {
-	std::ofstream xff_file(".xff");
-	for (const std::filesystem::directory_entry& entry:
-			std::filesystem::recursive_directory_iterator("."))
+	std::ifstream xff_ifile(".xff");
+	if (!xff_ifile.is_open())
 	{
-		if (entry.is_regular_file())
-		{
-			const std::filesystem::path& path = entry.path();
-			const std::filesystem::path& ext = path.extension();
-			std::shared_ptr<File> file;
-			if (ext == ".txt")
-				file = std::make_shared<TXTFile>(path);
-			else if (ext == ".csv")
-				file = std::make_shared<CSVFile>(CSVFile(path));
-			else if (ext == ".cpp")
-				file = std::make_shared<CPPFile>(path);
-			else
-				file = std::make_shared<File>(File(path));
-			file->store(xff_file);
-		}
+		std::cout << "Failed to open xff_ifile" << std::endl;
+		return;
 	}
-	xff_file.close();
+	std::stringstream buffer;
+	buffer << xff_ifile.rdbuf();
+	std::string file_contents = buffer.str();
+	std::istringstream iss(file_contents);
+	int counter = 1;
+	while (iss.rdbuf()->in_avail())
+	{
+		File f(iss);
+		std::shared_ptr<File> file;
+		const std::filesystem::path& ext = f.extension();
+		if (ext == ".cpp")
+			file = std::make_shared<CPPFile>(f, iss);
+		else if (ext == ".txt")
+			file = std::make_shared<TXTFile>(f, iss);
+		else if (ext == ".csv")
+			file = std::make_shared<CSVFile>(f, iss);
+		else
+			file = std::make_shared<File>(f);
 
+		if (query->evaluate(file))
+			file->print(std::cout << counter++ << ") ") << std::endl;
+	}
+}
+
+std::shared_ptr<MainQuery> create_query(int argc, char* const* argv)
+{
 	if (argc % 3 != 0)
 		throw std::logic_error("Invalid number of arguments");
 
@@ -70,33 +79,37 @@ int main(int argc, char** argv)
 		query->add(parse(argv[i], argv[i + 1]));
 		i += 2;
 	}
+	return query;
+}
 
-	std::ifstream xff_ifile(".xff");
-	if (!xff_ifile.is_open())
+void create_index(std::ofstream& xff_file)
+{
+	for (const std::filesystem::directory_entry& entry:
+			std::filesystem::recursive_directory_iterator("."))
 	{
-		std::cout << "Failed to open xff_ifile" << std::endl;
-		return 1;
+		if (entry.is_regular_file())
+		{
+			const std::filesystem::path& path = entry.path();
+			const std::filesystem::path& ext = path.extension();
+			std::shared_ptr<File> file;
+			if (ext == ".txt")
+				file = std::make_shared<TXTFile>(path);
+			else if (ext == ".csv")
+				file = std::make_shared<CSVFile>(CSVFile(path));
+			else if (ext == ".cpp")
+				file = std::make_shared<CPPFile>(path);
+			else
+				file = std::make_shared<File>(File(path));
+			file->store(xff_file);
+		}
 	}
-	std::stringstream buffer;
-	buffer << xff_ifile.rdbuf();
-	std::string file_contents = buffer.str();
-	std::istringstream iss(file_contents);
-	int counter = 1;
-	while (iss.rdbuf()->in_avail())
-	{
-		File f(iss);
-		std::shared_ptr<File> file;
-		const std::filesystem::path& ext = f.extension();
-		if (ext == ".cpp")
-			file = std::make_shared<CPPFile>(f, iss);
-		else if (ext == ".txt")
-			file = std::make_shared<TXTFile>(f, iss);
-		else if (ext == ".csv")
-			file = std::make_shared<CSVFile>(f, iss);
-		else
-			file = std::make_shared<File>(f);
+	xff_file.close();
+}
 
-		if (query->evaluate(file))
-			file->print(std::cout << counter++ << ") ") << std::endl;
-	}
+int main(int argc, char** argv)
+{
+	std::ofstream xff_file(".xff");
+	create_index(xff_file);
+	std::shared_ptr<MainQuery> query = create_query(argc, argv);
+	load_and_print(query);
 }
