9fb94e812c4321d9bdc0e0a04f1a2ed0e62c66ff
Tommy Chu <chutommy@fit.cvut.cz>
2023-05-14 10:49:00 +0200

    Add index class

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ std::shared_ptr<MainQuery> create_query(int argc, char* const* argv)
 
 	std::shared_ptr<MainQuery> query;
 	bool conjunction = true, disjunction = true;
-	for (size_t i = 3; i < argc; i += 3)
+	for (int i = 3; i < argc; i += 3)
 	{
 		if (conjunction && strcmp(argv[i], "and") != 0)
 			conjunction = false;
@@ -66,7 +66,7 @@ std::shared_ptr<MainQuery> create_query(int argc, char* const* argv)
 	else
 		throw std::logic_error("Invalid logical chaining");
 
-	for (size_t i = 1; i < argc; i++)
+	for (int i = 1; i < argc; i++)
 	{
 		size_t remaining = argc - i;
 		if (remaining == 0)
@@ -79,90 +79,12 @@ std::shared_ptr<MainQuery> create_query(int argc, char* const* argv)
 	return query;
 }
 
-void index_filepath(std::ofstream& os, const std::filesystem::path& path)
-{
-	const std::filesystem::path& ext = path.extension();
-	std::shared_ptr<File> file;
-	if (ext == ".txt")
-		file = std::make_shared<TXTFile>(path);
-	else if (ext == ".csv")
-		file = std::make_shared<CSVFile>(CSVFile(path));
-	else if (ext == ".cpp")
-		file = std::make_shared<CPPFile>(path);
-	else
-		file = std::make_shared<File>(File(path));
-	file->store(os);
-}
-
 int main(int argc, char** argv)
 {
-	Index index(".dummy");
+	Logger logger(std::cout);
+	Index index(".", logger);
+	index.update();
 
-	std::ifstream orig_xff(".xff");
-	std::ofstream new_xff(".xff.new");
-	if (!orig_xff.is_open() || !new_xff.is_open())
-	{
-		std::cout << "Failed to open index file" << std::endl;
-		return 1;
-	}
-
-	std::stringstream buffer;
-	buffer << orig_xff.rdbuf();
-	std::string index_str = buffer.str();
-	std::istringstream iss(index_str);
-
-	std::set<std::string> indexed;
-
-	while (iss.rdbuf()->in_avail())
-	{
-		File f(iss);
-		std::shared_ptr<File> file;
-		const std::filesystem::path& ext = f.extension();
-		if (ext == ".cpp")
-			file = std::make_shared<CPPFile>(f, iss);
-		else if (ext == ".txt")
-			file = std::make_shared<TXTFile>(f, iss);
-		else if (ext == ".csv")
-			file = std::make_shared<CSVFile>(f, iss);
-		else
-			file = std::make_shared<File>(f);
-
-		std::filesystem::path path = file->get_path();
-		if (!exists(path))
-		{
-			std::cout << "removed: " << path.filename() << std::endl;
-			continue;
-		}
-		if (!is_regular_file(path))
-		{
-			std::cout << "not supported: " << path.filename() << std::endl;
-			continue;
-		}
-		if (file->up_to_date())
-		{
-			file->store(new_xff);
-			indexed.insert(absolute(path));
-		}
-		else
-		{
-			std::cout << "reindexing: " << path.filename() << std::endl;
-			index_filepath(new_xff, path);
-			indexed.insert(absolute(path));
-		}
-	}
-
-	for (const std::filesystem::directory_entry& entry:
-			std::filesystem::recursive_directory_iterator("."))
-	{
-		const std::filesystem::path& path = entry.path();
-		if (is_regular_file(path) && !indexed.count(absolute(path)))
-		{
-			std::cout << "indexing: " << path.filename() << std::endl;
-			index_filepath(new_xff, path);
-		}
-	}
-
-//	create_index(orig_xff);
 //	std::shared_ptr<MainQuery> query = create_query(argc, argv);
 //	load_and_print(query);
 }
