7d49a77502b5963ce203adc3ffade264f87dcb67
Tommy Chu <chutommy@fit.cvut.cz>
2023-05-15 09:04:33 +0200

    Implement root class

---
+++ src/Root.h
@@ -0,0 +1,32 @@
+/**
+ * @author Tommy Chu <chutommy@fit.cvut.cz>
+ * @date 15.5.23
+ */
+
+#pragma once
+
+#include <filesystem>
+#include "Logger.h"
+#include "Index.h"
+
+/**
+ * Handles command-line actions.
+ */
+class Root
+{
+private:
+	std::filesystem::path root_dir;
+	Logger logger;
+	Index index;
+
+public:
+	Root(const std::string& new_root_dir, std::ostream& ostream);
+
+	/**
+	 * Executes command-line arguments.
+	 * @param argc argument count from console
+	 * @param argv argument vector from console
+	 * @return command-line return code
+	 */
+	int run(int argc, char** argv) const;
+};
